35949b86e995ed0bad061382a8c1cfc3dd309994
hanktwichell <78049692+hanktwichell@users.noreply.github.com>
2022-11-08 15:08:39 -0800

    Delete WebDevelopment/5.Likes directory

--- WebDevelopment/5.Likes/LikesPartI.js
+++
@@ -1,9 +0,0 @@
-var numLikes = 0;
-function addLike() {
-    var likes = document.querySelector("#num-likes");
-    numLikes++;
-    if (numLikes == 1)
-        likes.innerHTML = numLikes + " Like";
-    else
-        likes.innerHTML = numLikes + " Likes";
-}
