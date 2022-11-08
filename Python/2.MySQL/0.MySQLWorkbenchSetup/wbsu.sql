335c85c7349ebdfa8cec5c5371900876e8378612
hanktwichell <78049692+hanktwichell@users.noreply.github.com>
2022-11-08 15:34:45 -0800

    Add files via upload

---
+++ Python/2.MySQL/0.MySQLWorkbenchSetup/wbsu.sql
@@ -0,0 +1,13 @@
+use names;
+
+SELECT * 
+FROM names;
+
+INSERT INTO names (id, name) 
+VALUES(1, 'Hank');
+
+INSERT INTO names (id, name)
+VALUES(2, 'Jonathan');
+
+-- UPDATE names
+-- SET name = 'First Name';
